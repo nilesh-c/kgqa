15affefcd19fab66a93e728e3fc879520d6cccb6
Nilesh Chakraborty <nilesh@nileshc.com>
2019-07-01 15:11:53 +0100

    Setup executors in base testcase

--- kgqa/common/testcase.py
+++ kgqa/common/testcase.py
@@ -2,16 +2,20 @@ from unittest import TestCase
 import pathlib
 
 from hdt import HDTDocument
+
+from kgqa.semparse.executor.stub_executor import StubExecutor
 from kgqa.semparse.executor.hdt_executor import HdtExecutor
 
 class KGQATestCase(TestCase):
-    PROJECT_ROOT = (pathlib.Path(__file__).parent / ".." / ".." / "..").resolve()  # pylint: disable=no-member
-    MODULE_ROOT = PROJECT_ROOT / "dialogue_models"
-    TOOLS_ROOT = MODULE_ROOT / "tools"
-    TESTS_ROOT = MODULE_ROOT / "tests"
-    FIXTURES_ROOT = TESTS_ROOT / "fixtures"
+    PROJECT_ROOT = (pathlib.Path(__file__).parent / ".." / "..").resolve()  # pylint: disable=no-member
+    MODULE_ROOT = PROJECT_ROOT / "kgqa"
+    TEST_DATA_ROOT = MODULE_ROOT / "tests" / "data"
 
     @classmethod
-    def setUpClass(self):
+    def setUpExecutor(self):
         hdt = HDTDocument('/data/nilesh/datasets/dbpedia/hdt/dbpedia2016-04en.hdt', map=True, progress=True)
         self.executor = HdtExecutor(graph=hdt)
+
+    @classmethod
+    def setUpStubExecutor(self):
+        self.executor = StubExecutor()
